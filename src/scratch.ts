b6ab811779c9b2ccf79529b89f2d6d5de6c845b6
Coby <contact@45930.xyz>
2024-01-17 09:23:26 -0500

    use jwt lib for sign/verify

--- src/scratch.ts
+++ src/scratch.ts
@@ -1,37 +1,49 @@
-import { Field, PrivateKey, PublicKey, Signature, EcdsaSignature } from "o1js";
-import { encode, decode } from '@web3pack/base58-check';
-import { getHashes } from "node:crypto";
+import { Field, PrivateKey, PublicKey, Signature } from "o1js";
+import jwt from "jsonwebtoken";
+
+const P256_KEY = 
+`-----BEGIN EC PARAMETERS-----
+BggqhkjOPQMBBw==
+-----END EC PARAMETERS-----
+-----BEGIN EC PRIVATE KEY-----
+MHcCAQEEIEAhT7cezMG3YX6uow+tr+PYYIQpddChtD+4kQKCdaPSoAoGCCqGSM49
+AwEHoUQDQgAEb20XDk/dNdY8Pyj58dfHJl+13ngUMU8LVt2nezm93XKX5rr3+I2/
+GEjHle+84NSvOcI64kfru+vfwhokimpFBw==
+-----END EC PRIVATE KEY-----
+`
+
+const P256_PUBLIC_KEY = 
+`
+-----BEGIN PUBLIC KEY-----
+MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEb20XDk/dNdY8Pyj58dfHJl+13ngU
+MU8LVt2nezm93XKX5rr3+I2/GEjHle+84NSvOcI64kfru+vfwhokimpFBw==
+-----END PUBLIC KEY-----
+`
 
 const minaKeyStr = 'EKE2GXDY89E4H64VFwkuqovH8VsFxeXHnvU1gmZwZniPsAqDARC4';
 
 const minaKey = PrivateKey.fromBase58(minaKeyStr);
 const subject = minaKey.toPublicKey();
 const header = {
-  alg: "HS256",
-  typ: 'MWT'
+  alg: "ES256",
+  typ: 'JWT'
 }
 const payload = {
   sub: subject.toBase58()
 }
 
-const b64Header = btoa(JSON.stringify(header))
-const b64Payload = btoa(JSON.stringify(payload))
-const sig = EcdsaSignature.sign(
-  Uint8Array.from(JSON.stringify(header).split("").map(x => x.charCodeAt(0))),
-  minaKey.toBigInt()
-)
-const b64Sig = btoa(JSON.stringify(sig.toBigInt()))
+const token = jwt.sign(payload, P256_KEY, { header })
 
-const mwt = b64Header + '.' +
-b64Payload + '.' +
-b64Sig
+console.log(token);
 
-console.log(mwt);
-
-const [encodedHeader, encodedPayload, hmac] = mwt.split('.');
+const [encodedHeader, encodedPayload, mac] = token.split('.');
 const decodedHeader = atob(encodedHeader);
 const decodedPayload = atob(encodedPayload);
-// const verify = createVerify('hmac')
 
-console.log(getHashes());
+console.log(decodedHeader);
+console.log(decodedPayload);
+console.log(mac);
 
+const decoded = jwt.verify(token, P256_PUBLIC_KEY)
+
+console.log(decoded);
